fab28e5e91c44ef7f82d0277adecb2cc2d548489
Zhenyu Wang <2441461233@qq.com>
2020-11-20 20:34:39 +0800

    zy_02

--- zy/miniP-SE/pages/index/index.js
+++ zy/miniP-SE/pages/index/index.js
@@ -1,54 +1,50 @@
-//index.js
-//获取应用实例
-const app = getApp()
-
+//Page Object
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    // 轮播图数据
+    swipper_list: [],
+    // 楼层数据——推荐
+    floor_list: [
+      {
+        id: 0,
+        src: "https://pic4.zhimg.com/80/v2-467c992d6a2dd5739f1bd1b381b03b57_1440w.jpg",
+        recommend:"⌚超好用的计时器"
+      },
+      {
+        id: 1,
+        src: "https://ci.xiaohongshu.com/60ee5f2f-9e02-81b7-1ef8-53145544cf7d?imageView2/2/w/540/format.jpg",
+        recommend:"💄各色口红"
+      },
+      {
+        id: 2,
+        src: "//img.alicdn.com/imgextra/i4/1980545515/TB2r2RUo3ZC2uNjSZFnXXaxZpXa_!!1980545515-0-beehive-scenes.jpg_360x360xzq90.jpg_.webp",
+        recommend:"💄一眼就爱上的口红"
+      },
+      {
+        id: 3,
+        src: "https://ci.xiaohongshu.com/96f4bbb6-b927-a734-11f4-98894a8bfc4e?imageView2/2/w/540/format.jpg",
+        recommend:"🍼男士防坑洗面奶"
+      },
+      {
+        id: 4,
+        src: "https://pic2.zhimg.com/80/v2-9aa5c0ca5c5709824e54e14d0bfc0c5d_1440w.jpg",
+        recommend:"超好用的书支架"
+      }
+    ]
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
+  //options(Object)
+  onLoad: function(options){
+    wx.request({
+      url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata',
+      method: 'GET',
+      dataType: 'json',
+      responseType: 'text',
+      success: (result)=>{
+        // console.log(result.data.message);
         this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
+          swipper_list: result.data.message
         })
       }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
+    });
   }
-})
+});
